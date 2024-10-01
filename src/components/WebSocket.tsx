5281f80358ab07b1273048953c171c565a891aa2
Mitchell Mahoney <mahoneymitch@gmail.com>
2024-10-01 16:18:31 -0400

    Use env values

--- src/components/WebSocket.tsx
+++ src/components/WebSocket.tsx
@@ -10,8 +10,8 @@ export default function WebSocketComponent({ uuid, onPlayed }: webSocketProps) {
   useEffect(() => {
     // Make sure this runs on the client side
     if (typeof window !== "undefined") {
-      const pusher = new Pusher("abec2a31dd56a1cb649d", {
-        cluster: "us2",
+      const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
       });
       const channel = pusher.subscribe(uuid);
       channel.bind("played", () => onPlayed());
--- websocket.ts
+++ websocket.ts
@@ -2,9 +2,9 @@ import Pusher from "pusher";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.PUSHER_KEY as string,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY as string,
   secret: process.env.PUSHER_SECRET as string,
-  cluster: process.env.PUSHER_CLUSTER as string,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
   useTLS: true,
 });
 
